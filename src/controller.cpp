903549fd604ff9db4ee23b889ab970310bad8bbe
lorenzo <lpizzutobeltran@itba.edu.ar>
2026-08-24 20:19:12 +0000

    cmake build tools

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,9 +1,9 @@
 #include "controller.hpp"
 
 void UpdateDemoController(const ControllerInput &input, TargetDrone &drone) {
-    static_cast<void>(input);
-    drone.SetMotorTarget(MotorId::FrontLeft, 0.8f);
-    drone.SetMotorTarget(MotorId::FrontRight, 0.8f);
-    drone.SetMotorTarget(MotorId::RearRight, 0.8f);
-    drone.SetMotorTarget(MotorId::RearLeft, 0.8f);
+  static_cast<void>(input);
+  drone.SetMotorTarget(MotorId::FrontLeft, 0.8f);
+  drone.SetMotorTarget(MotorId::FrontRight, 0.8f);
+  drone.SetMotorTarget(MotorId::RearRight, 0.8f);
+  drone.SetMotorTarget(MotorId::RearLeft, 0.8f);
 }
