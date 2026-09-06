88a3b5239db2f8b54a85058ea4545961327dd7bb
lorenzo <lpizzutobeltran@itba.edu.ar>
2026-09-06 20:29:58 +0000

    first Tricopter idea

---
+++ src/controllers/include/controllers/tricopter_angle_mode_controller.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "controllers/attitude_controller.hpp"
+
+class TricopterAngleModeController {
+public:
+  void Reset();
+  void Update(const ControllerInput &input, MotorCommands &motor_commands);
+
+  float GetThrottle() const;
+
+private:
+  AttitudeController attitude_controller_;
+  AttitudeSetpoint setpoint_{.throttle = 0.70f};
+};
