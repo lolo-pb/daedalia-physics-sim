e995d6338de6b422722a5953b76f2c6c65401bdd
lorenzo <lpizzutobeltran@itba.edu.ar>
2026-09-22 02:39:38 +0000

    Bypyramid drone defintitin, initial

--- src/drones/triangular-bipyramid-copter.cpp
+++ src/drones/triangular-bipyramid-copter.cpp
@@ -3,8 +3,9 @@
 
 DroneDefinition CreateTriangularBipyramidCopterDefinition() {
   return {
-      JPH::Vec3(0.25f, 0.08f, 0.25f), // half extent 
-      0.5f,// wheight
+      JPH::Vec3(0.577, 0.50, 0.816), // half extent, box that holds the 
+      //JPH::Vec3(0.096, 0.167, 0.272),
+      1.0f,// wheight
       JPH::RVec3(0.0, 1.0, 0.0),//start pos
       JPH::Quat::sIdentity(),   //start rPot
       {// list of motors
@@ -16,43 +17,43 @@ DroneDefinition CreateTriangularBipyramidCopterDefinition() {
         // 2.0e-8f     // reaction torque coefficient
         //},        
         {JPH::Vec3( 0.096,  0.167,  0.272),
-         JPH::Vec3(0.0f, 0.0f, 0.0f),
-         JPH::Vec3(0.0f, 0.0f, 0.0f),
-         1000.0f,
-         5.0e-6f,
-         2.0e-8f
-        },
-        {JPH::Vec3(-0.192,  0.000,  0.272),
-         JPH::Vec3(0.0f, 0.0f, 0.0f),
-         JPH::Vec3(0.0f, 0.0f, 0.0f),
-         1000.0f,
-         5.0e-6f,
-         2.0e-8f
-        },
-        {JPH::Vec3( 0.096, -0.167,  0.272),
-         JPH::Vec3(0.0f, 0.0f, 0.0f),
-         JPH::Vec3(0.0f, 0.0f, 0.0f),
-         1000.0f,
-         5.0e-6f,
-         2.0e-8f
-        },
-        {JPH::Vec3( 0.096,  0.167, -0.272),
-         JPH::Vec3(0.0f, 0.0f, 0.0f),
-         JPH::Vec3(0.0f, 0.0f, 0.0f),
-         1000.0f,
-         5.0e-6f,
-         2.0e-8f
-        },
-        {JPH::Vec3(-0.192,  0.000, -0.272),
-         JPH::Vec3(0.0f, 0.0f, 0.0f),
-         JPH::Vec3(0.0f, 0.0f, 0.0f),
-         1000.0f,
-         5.0e-6f,
-         2.0e-8f
-        },
-        {JPH::Vec3( 0.096, -0.167, -0.272),
-         JPH::Vec3(0.0f, 0.0f, 0.0f),
-         JPH::Vec3(0.0f, 0.0f, 0.0f),
+          JPH::Vec3( 0.4714f, 0.8165f, 0.3333f),
+          JPH::Vec3( 0.4714f, 0.8165f, 0.3333f),
+          1000.0f,
+          5.0e-6f,
+          2.0e-8f
+         },
+         {JPH::Vec3(-0.192,  0.000,  0.272),
+          JPH::Vec3(-0.9426f, 0.0000f, 0.3338f),
+          JPH::Vec3(-0.9426f, 0.0000f, 0.3338f),
+          1000.0f,
+          5.0e-6f,
+          2.0e-8f
+         },
+         {JPH::Vec3( 0.096, -0.167,  0.272),
+          JPH::Vec3( 0.4714f,-0.8165f, 0.3333f),
+          JPH::Vec3( 0.4714f,-0.8165f, 0.3333f),
+          1000.0f,
+          5.0e-6f,
+          2.0e-8f
+         },
+         {JPH::Vec3( 0.096,  0.167, -0.272),
+          JPH::Vec3( 0.4714f, 0.8165f,-0.3333f),
+          JPH::Vec3( 0.4714f, 0.8165f,-0.3333f),
+          1000.0f,
+          5.0e-6f,
+          2.0e-8f
+         },
+         {JPH::Vec3(-0.192,  0.000, -0.272),
+          JPH::Vec3(-0.9426f, 0.0000f,-0.3338f),
+          JPH::Vec3(-0.9426f, 0.0000f,-0.3338f),
+          1000.0f,
+          5.0e-6f,
+          2.0e-8f
+         },
+         {JPH::Vec3( 0.096, -0.167, -0.272),
+         JPH::Vec3( 0.4714,-0.8165,-0.3333),
+         JPH::Vec3( 0.4714,-0.8165,-0.3333),
          1000.0f,
          5.0e-6f,
          2.0e-8f
@@ -71,10 +72,11 @@ Top apex:      ( 0.000,  0.000,  0.816)
 Bottom apex:   ( 0.000,  0.000, -0.816)
 
 //aca van los motores
-Upper face 1: ( 0.096,  0.167,  0.272)X
-Upper face 2: (-0.192,  0.000,  0.272)X
-Upper face 3: ( 0.096, -0.167,  0.272)X
-Lower face 1: ( 0.096,  0.167, -0.272)X
-Lower face 2: (-0.192,  0.000, -0.272)X
-Lower face 3: ( 0.096, -0.167, -0.272)X
+Upper face 1: ( 0.096,  0.167,  0.272)X normal ( 0.4714, 0.8165, 0.3333)
+Upper face 2: (-0.192,  0.000,  0.272)X normal (-0.9426, 0.0000, 0.3338)
+Upper face 3: ( 0.096, -0.167,  0.272)X normal ( 0.4714,-0.8165, 0.3333)
+Lower face 1: ( 0.096,  0.167, -0.272)X normal ( 0.4714, 0.8165,-0.3333)
+Lower face 2: (-0.192,  0.000, -0.272)X normal (-0.9426, 0.0000,-0.3338)
+Lower face 3: ( 0.096, -0.167, -0.272)X normal ( 0.4714,-0.8165,-0.3333)X
+
 */
